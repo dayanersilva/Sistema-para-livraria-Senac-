573d6fd0ec79b13f3415921ffcc57dbc4bda7628
dayanersilva <dayane.rodrigues_silva@hotmail.com>
2021-11-22 21:51:38 -0300

    conexão BD

--- backend/cadastrar-livro.php
+++ backend/cadastrar-livro.php
@@ -1,10 +1,23 @@
 <?php
 //mostrar dados  recebidos do front via POST
 //var_dump($_POST);
-$titulo = $_POST['titulo'];
 
-echo $titulo;
+try {
+    //captura os valores recebidos via POST e guarda nas variaveis especificas 
+    $titulo    = $_POST['titulo'];
+    $isbn      = $_POST['isbn'];
+    $autor     = $_POST['autor'];
+    $categoria = $_POST['categoria'];
+    $editora   = $_POST['editora'];
+    $valor     = $_POST['valor'];
+    
+    //Inclui o arquivo de conexão com os Bancos de Dados 
 
+
+    
+} catch (PDOException $erro) {
+    
+}
 
 
 
---
+++ backend/conexao.php
@@ -0,0 +1,23 @@
+<?php
+
+    // variaveis com os dados da conexão com o servidor 
+    $servidor_bd    = 'localhost';
+    $usuario_bd     = 'root';
+    $senha_bd       = '';
+    $database_bd    = 'db_livraria';
+
+
+    try{
+        //realiza a conexão com o banco de dados
+        $conexao = new PDO("mysql:host=$servidor_bd;dbname=$database_bd;charset=utf8",$usuario_bd,$senha_bd);
+        $conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        //echo 'Conectado';
+
+    }catch (PDOException $erro){
+        //caso a conexão mão seja realizada, exibe o erro
+        echo 'Erro ao conectar no banco de dados: ' .$erro->getMessage();
+        exit();
+    }
+
+?>
+

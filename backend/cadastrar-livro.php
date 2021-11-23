6cf7263b05b387fb097f01c29be2143d2c8f0cd6
dayanersilva <dayane.rodrigues_silva@hotmail.com>
2021-11-22 21:34:07 -0300

    validação final inicio PHP

---
+++ backend/cadastrar-livro.php
@@ -0,0 +1,11 @@
+<?php
+//mostrar dados  recebidos do front via POST
+//var_dump($_POST);
+$titulo = $_POST['titulo'];
+
+echo $titulo;
+
+
+
+
+?>
--- js/cadastrar-livro.js
+++ js/cadastrar-livro.js
@@ -4,13 +4,75 @@ const cadastrar = () =>{
 
    //campo nome
    //captura o valor preenchido no INPUT titulo
-   let titulo =$('#titulo').val()
+   let titulo =$('#titulo').val();
+   let isbn = $('#isbn').val();
+   let autor = $('#autor').val();
+   let categoria = $('#categoria').val();
+   let editora = $('#editora').val();
+   let valor = $('#valor').val();
 
-   if(titulo==''){
-       $('#titulo').addClass('is-invalid')
+
+   if(titulo ==''){
+       $('#titulo').addClass('is-invalid');
        return
+   }else{
+    $('#titulo').removeClass('is-invalid')
+    $('#titulo').addClass('is-valid')
    }
 
-  // console.log(`titulo:${titulo}`)
+   if(isbn ==''){
+     $('#isbn').addClass('is-invalid');
+     return
+   }else{
+    $('#isbn').removeClass('is-invalid')
+    $('#isbn').addClass('is-valid')
+   }
+
+   if(autor ==''){
+     $('#autor').addClass('is-invalid');
+     return
+   }else{
+    $('#autor').removeClass('is-invalid')
+    $('#autor').addClass('is-valid')
+   }
+
+   if(categoria == null){
+     $('#categoria').addClass('is-invalid');
+     return
+   }else{
+    $('#categoria').removeClass('is-invalid')
+    $('#categoria').addClass('is-valid')
+   }
+
+   if(editora == null){
+    $('#editora').addClass('is-invalid');
+    return
+  }else{
+    $('#editora').removeClass('is-invalid')
+    $('#editora').addClass('is-valid')
+   }
+
+  if(valor == ''){
+    $('#valor').addClass('is-invalid');
+    return
+  }else{
+    $('#valor').removeClass('is-invalid')
+    $('#valor').addClass('is-valid')
+   }
+
+  console.log(`titulo:${titulo}, ISBN:${isbn}, Autor:${autor}, Categoria:${categoria}, Editora:${editora}, Valor:${valor}`)
+
+  //Final da validação JS
+
+  //Inicio de envio de dados para o PHP
+  //Serializar os dados do formulário para envio
+  // a variavel form, irá receber as chaves e valores preenchidos no form cadastrar
+  let form =  new FormData($('#form-cadastrar')[0])
+
+  //envios dos dados utiliando o FETCH
+  fetch('backend/cadastrar-livro.php',{
+    method: 'POST',
+    body: form 
+  })
 
 }

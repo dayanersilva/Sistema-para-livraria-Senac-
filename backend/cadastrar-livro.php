48d59a843f4d6ec9bc89f6253dbce625aa71b9bd
dayanersilva <dayane.rodrigues_silva@hotmail.com>
2021-11-24 21:33:59 -0300

    cadastrar livro backend finalizado

--- backend/cadastrar-livro.php
+++ backend/cadastrar-livro.php
@@ -15,9 +15,51 @@ try {
     include 'conexao.php'; 
 
 
-    
+// variavel que ira armazenar o comando SQL a ser executada no BD
+    $sql = "INSERT INTO tb_livro (
+                                    titulo,
+                                    isbn,
+                                    autor,
+                                    categoria,
+                                    editora,
+                                    valor) VALUES(
+                                    '$titulo ' ,
+                                    $isbn,
+                                    '$autor',
+                                    '$categoria',
+                                    '$editora',
+                                     '$valor'
+                                    );
+            ";
+
+    // debug da variavel SQL
+    // echo $sql;
+
+    //prepara a execução do comando $sql armazenado na variável 
+    $comando = $conexao->prepare($sql);
+
+    //executa o comando no banco de dados
+    $comando->execute();
+
+    //cria um array chave/valor para ser convertido em JSON e retornado ao FRONT
+    $retorno = array('retorno'=>'Sucesso', 'mensagem'=>'Cadastro realizado');
+
+    //transforma o array de retorno em formato JSON 
+    $json = json_encode($retorno, JSON_UNESCAPED_UNICODE);
+
+    //retorna o JSON ao Front
+    echo $json;
+
 } catch (PDOException $erro) {
-    
+    //cria um array chave/valor para ser convertido em JSON e retornado ao FRONT
+    $retorno = array('retorno'=>'Erro', 'mensagem'=>$erro->getMessage());
+
+    //transforma o array de retorno em formato JSON 
+    $json = json_encode($retorno, JSON_UNESCAPED_UNICODE);
+
+    //retorna o JSON ao Front
+    echo $json;
+
 }
 
 
